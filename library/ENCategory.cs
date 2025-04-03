43fae9625d4265a2011569fdb44736085af83a32
BrunoCámaraMoral <bruno20051121@gmail.com>
2025-04-03 20:09:06 +0200

    77044609Z encategory y camcio enproduct

---
+++ library/ENCategory.cs
@@ -0,0 +1,44 @@
+﻿namespace library
+{
+    public class ENCategory
+    {
+        // Atributos privados
+        private int id;
+        private string name;
+
+        // Propiedades públicas con campos de respaldo
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        // Constructores
+        public ENCategory() { }
+
+        public ENCategory(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        // Métodos para acceso a datos
+        public bool Read()
+        {
+            CADCategory cad = new CADCategory();
+            return cad.Read(this);
+        }
+
+        public static List<ENCategory> ReadAll()
+        {
+            CADCategory cad = new CADCategory();
+            return cad.ReadAll();
+        }
+    }
+}
--- library/ENProduct.cs
+++ library/ENProduct.cs
@@ -5,9 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-
-using System;
-
 namespace library
 {
     public class ENProduct

a148dc26a57e88ca175a81dd594c38270891481b
Jose <estarlichjm@gmail.com>
2025-04-03 23:37:35 +0200

    20519186N Creados esqueletos básicos de ENProduct y CADProduct

--- library/CADProduct.cs
+++ library/CADProduct.cs
@@ -3,10 +3,212 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
 
 namespace library
 {
     public class CADProduct
     {
+        private string constring;
+
+        public CADProduct()
+        {
+            constring = System.Configuration.ConfigurationManager.ConnectionStrings["DatabaseConnectionString"].ToString();
+        }
+
+        public bool Create(ENProduct en)
+        {
+            try
+            {
+                SqlConnection c = new SqlConnection(constring);
+                c.Open();
+                SqlCommand com = new SqlCommand("INSERT INTO Products (name, code, amount, price, category, creationDate) VALUES (@name, @code, @amount, @price, @category, @creationDate)", c);
+                com.Parameters.AddWithValue("@name", en.Name);
+                com.Parameters.AddWithValue("@code", en.Code);
+                com.Parameters.AddWithValue("@amount", en.Amount);
+                com.Parameters.AddWithValue("@price", en.Price);
+                com.Parameters.AddWithValue("@category", en.Category);
+                com.Parameters.AddWithValue("@creationDate", en.CreationDate);
+                com.ExecuteNonQuery();
+                c.Close();
+                return true;
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Product operation has failed. Error: {0}", ex.Message);
+                return false;
+            }
+        }
+
+        public bool Update(ENProduct en)
+        {
+            try
+            {
+                SqlConnection c = new SqlConnection(constring);
+                c.Open();
+                SqlCommand com = new SqlCommand("UPDATE Products SET name=@name, amount=@amount, price=@price, category=@category, creationDate=@creationDate WHERE code=@code", c);
+                com.Parameters.AddWithValue("@name", en.Name);
+                com.Parameters.AddWithValue("@amount", en.Amount);
+                com.Parameters.AddWithValue("@price", en.Price);
+                com.Parameters.AddWithValue("@category", en.Category);
+                com.Parameters.AddWithValue("@creationDate", en.CreationDate);
+                com.Parameters.AddWithValue("@code", en.Code);
+                com.ExecuteNonQuery();
+                c.Close();
+                return true;
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Product operation has failed. Error: {0}", ex.Message);
+                return false;
+            }
+        }
+
+        public bool Delete(ENProduct en)
+        {
+            try
+            {
+                SqlConnection c = new SqlConnection(constring);
+                c.Open();
+                SqlCommand com = new SqlCommand("DELETE FROM Products WHERE code=@code", c);
+                com.Parameters.AddWithValue("@code", en.Code);
+                com.ExecuteNonQuery();
+                c.Close();
+                return true;
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Product operation has failed. Error: {0}", ex.Message);
+                return false;
+            }
+        }
+
+        public bool Read(ENProduct en)
+        {
+            try
+            {
+                SqlConnection c = new SqlConnection(constring);
+                c.Open();
+                SqlCommand com = new SqlCommand("SELECT * FROM Products WHERE code=@code", c);
+                com.Parameters.AddWithValue("@code", en.Code);
+                SqlDataReader dr = com.ExecuteReader();
+                if (dr.Read())
+                {
+                    en.Name = dr["name"].ToString();
+                    en.Amount = Convert.ToInt32(dr["amount"]);
+                    en.Price = Convert.ToSingle(dr["price"]);
+                    en.Category = Convert.ToInt32(dr["category"]);
+                    en.CreationDate = Convert.ToDateTime(dr["creationDate"]);
+                    dr.Close();
+                    c.Close();
+                    return true;
+                }
+                dr.Close();
+                c.Close();
+                return false;
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Product operation has failed. Error: {0}", ex.Message);
+                return false;
+            }
+        }
+
+        public bool ReadFirst(ENProduct en)
+        {
+            try
+            {
+                SqlConnection c = new SqlConnection(constring);
+                c.Open();
+                SqlCommand com = new SqlCommand("SELECT TOP 1 * FROM Products ORDER BY id ASC", c);
+                SqlDataReader dr = com.ExecuteReader();
+                if (dr.Read())
+                {
+                    en.Code = dr["code"].ToString();
+                    en.Name = dr["name"].ToString();
+                    en.Amount = Convert.ToInt32(dr["amount"]);
+                    en.Price = Convert.ToSingle(dr["price"]);
+                    en.Category = Convert.ToInt32(dr["category"]);
+                    en.CreationDate = Convert.ToDateTime(dr["creationDate"]);
+                    dr.Close();
+                    c.Close();
+                    return true;
+                }
+                dr.Close();
+                c.Close();
+                return false;
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Product operation has failed. Error: {0}", ex.Message);
+                return false;
+            }
+        }
+
+        public bool ReadNext(ENProduct en)
+        {
+            try
+            {
+                SqlConnection c = new SqlConnection(constring);
+                c.Open();
+                SqlCommand com = new SqlCommand("SELECT TOP 1 * FROM Products WHERE code > @code ORDER BY code ASC", c);
+                com.Parameters.AddWithValue("@code", en.Code);
+                SqlDataReader dr = com.ExecuteReader();
+                if (dr.Read())
+                {
+                    en.Code = dr["code"].ToString();
+                    en.Name = dr["name"].ToString();
+                    en.Amount = Convert.ToInt32(dr["amount"]);
+                    en.Price = Convert.ToSingle(dr["price"]);
+                    en.Category = Convert.ToInt32(dr["category"]);
+                    en.CreationDate = Convert.ToDateTime(dr["creationDate"]);
+                    dr.Close();
+                    c.Close();
+                    return true;
+                }
+                dr.Close();
+                c.Close();
+                return false;
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Product operation has failed. Error: {0}", ex.Message);
+                return false;
+            }
+        }
+
+        public bool ReadPrev(ENProduct en)
+        {
+            try
+            {
+                SqlConnection c = new SqlConnection(constring);
+                c.Open();
+                SqlCommand com = new SqlCommand("SELECT TOP 1 * FROM Products WHERE code < @code ORDER BY code DESC", c);
+                com.Parameters.AddWithValue("@code", en.Code);
+                SqlDataReader dr = com.ExecuteReader();
+                if (dr.Read())
+                {
+                    en.Code = dr["code"].ToString();
+                    en.Name = dr["name"].ToString();
+                    en.Amount = Convert.ToInt32(dr["amount"]);
+                    en.Price = Convert.ToSingle(dr["price"]);
+                    en.Category = Convert.ToInt32(dr["category"]);
+                    en.CreationDate = Convert.ToDateTime(dr["creationDate"]);
+                    dr.Close();
+                    c.Close();
+                    return true;
+                }
+                dr.Close();
+                c.Close();
+                return false;
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Product operation has failed. Error: {0}", ex.Message);
+                return false;
+            }
+        }
     }
 }
--- library/ENProduct.cs
+++ library/ENProduct.cs
@@ -6,7 +6,67 @@ using System.Threading.Tasks;
 
 namespace library
 {
-    internal class ENProduct
+    public class ENProduct
     {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public int Amount { get; set; }
+        public float Price { get; set; }
+        public int Category { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public ENProduct() { }
+
+        public ENProduct(string code, string name, int amount, float price, int category, DateTime creationDate)
+        {
+            this.Code = code;
+            this.Name = name;
+            this.Amount = amount;
+            this.Price = price;
+            this.Category = category;
+            this.CreationDate = creationDate;
+        }
+
+        public bool Create()
+        {
+            CADProduct cad = new CADProduct();
+            return cad.Create(this);
+        }
+
+        public bool Update()
+        {
+            CADProduct cad = new CADProduct();
+            return cad.Update(this);
+        }
+
+        public bool Delete()
+        {
+            CADProduct cad = new CADProduct();
+            return cad.Delete(this);
+        }
+
+        public bool Read()
+        {
+            CADProduct cad = new CADProduct();
+            return cad.Read(this);
+        }
+
+        public bool ReadFirst()
+        {
+            CADProduct cad = new CADProduct();
+            return cad.ReadFirst(this);
+        }
+
+        public bool ReadNext()
+        {
+            CADProduct cad = new CADProduct();
+            return cad.ReadNext(this);
+        }
+
+        public bool ReadPrev()
+        {
+            CADProduct cad = new CADProduct();
+            return cad.ReadPrev(this);
+        }
     }
 }
